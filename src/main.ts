b8b6ec26388481da227b5ef91f81541acfe8be15
Robert Arteaga <daspanisharmada@gmail.com>
2020-06-18 00:10:34 -0700

    added global api prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// The entry file of the application which uses the core function NestFactory to create a Nest application instance
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // prefix global routing with /api
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
