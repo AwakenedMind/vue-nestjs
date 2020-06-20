c3a8c67e55abdad7c27d25f179a37fdd2e1ab2f4
Robert Arteaga <daspanisharmada@gmail.com>
2020-06-19 23:37:42 -0700

    app module comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,10 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { BooksModule } from './books/books.module';
 
-/*
-  The @Module() decorator provides metadata that Nest makes use of to organize the application structure.
-  app.module.ts serves as the root module of the app
-*/
+/**
+ *   @desc   The @Module() decorator provides metadata that Nest makes use of to organize the application structure.
+ *           app.module.ts serves as the root module of the app
+ */
 @Module({
   imports: [
     // In order to serve static content for a SPA, we can use the ServeStaticModule to hook up VueJS
