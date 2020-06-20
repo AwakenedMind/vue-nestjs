9d6dd0f2e1b1aacb62c903990d41ece5d3cfc515
Robert Arteaga <daspanisharmada@gmail.com>
2020-06-20 00:14:55 -0700

    removed comments

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -5,21 +5,12 @@ import { BOOKS } from '../mocks/books.mock';
 export class BooksService {
   books = BOOKS;
 
-  /**
-   * Gets books
-   * @returns books
-   */
   getBooks(): Promise<any> {
     return new Promise(resolve => {
       resolve(this.books);
     });
   }
 
-  /**
-   * Gets book
-   * @param bookID
-   * @returns book
-   */
   getBook(bookID: number): Promise<any> {
     console.log(bookID);
     return new Promise(resolve => {
@@ -30,12 +21,9 @@ export class BooksService {
       resolve(book);
     });
   }
-  /**
-   * Adds book
-   * @param book
-   * @returns book
-   */
+
   addBook(book): Promise<any> {
+    console.log(book);
     return new Promise(resolve => {
       this.books.push(book);
       resolve(this.books);
@@ -43,7 +31,6 @@ export class BooksService {
   }
 
   deleteBook(bookID: number): Promise<any> {
-    console.log(bookID);
     return new Promise(resolve => {
       let index = this.books.findIndex(book => book.id === bookID);
       if (index === -1) {
