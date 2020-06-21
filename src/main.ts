e5337e93a83eafb3285b69ab14ab2c70731ff3c9
Robert Arteaga <daspanisharmada@gmail.com>
2020-06-20 22:22:32 -0700

    added cors in nestjs

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 // The entry file of the application which uses the core function NestFactory to create a Nest application instance
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // prefix global routing with /api
   app.setGlobalPrefix('api');
